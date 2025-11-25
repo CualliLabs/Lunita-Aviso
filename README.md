13b21cbf557e2754298380fe83c816b8d2d43421
Tuysh <angelmacloviomorales@gmail.com>
2025-11-25 11:43:29 -0600

    Primer commit :D

---
+++ README.md
@@ -0,0 +1,3 @@
+# lunita-aviso
+
+Pagina de aviso sobre el proyecto Lunita
---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.getElementById("aviso").addEventListener("click", function() {
+    alert("¡Gracias por tu interés! pero el programador está ocupado jugando con gatos y no ha programado esto 🐱🔮");
+});
